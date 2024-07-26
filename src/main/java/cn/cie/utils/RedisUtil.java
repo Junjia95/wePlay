1cba0e2f6bc1155e2a932e057620496d8d496e12
Junjia Wang <roubaoziwang@gmail.com>
2024-07-26 10:41:27 +0800

    add method setString, getString, setObj and getObj in class RedisUtil

---
+++ src/main/java/cn/cie/utils/RedisUtil.java
@@ -0,0 +1,46 @@
+package cn.cie.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtil {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    public void setString(String key, String value){
+        stringRedisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getString(String key){
+        String s = stringRedisTemplate.opsForValue().get(key);
+        return s;
+    }
+
+    public <T> void setObj(String key, T obj){
+        try {
+            String value = MAPPER.writeValueAsString(obj);
+            stringRedisTemplate.opsForValue().set(key, value);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T getObj(String key, T obj){
+        try {
+            String value = stringRedisTemplate.opsForValue().get(key);
+            obj = (T)MAPPER.readValue(value, obj.getClass());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return obj;
+    }
+
+}
--- src/test/java/cn/cie/WePlayApplicationTests.java
+++ src/test/java/cn/cie/WePlayApplicationTests.java
@@ -2,9 +2,14 @@ package cn.cie;
 
 import cn.cie.entity.User;
 import cn.cie.mapper.UserMapper;
+import cn.cie.utils.RedisUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.List;
 
@@ -12,6 +17,12 @@ import java.util.List;
 class WePlayApplicationTests {
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Test
     void contextLoads() {
@@ -23,6 +34,32 @@ class WePlayApplicationTests {
 
         User user = userMapper.selectById(1);
         System.out.println(user);
+    }
+
+    @Test
+    void testString(){
+        stringRedisTemplate.opsForValue().set("name", "zhangsan");
+        String name = stringRedisTemplate.opsForValue().get("name");
+        System.out.println(name);
+    }
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    @Test
+    void testSaveUser() throws JsonProcessingException {
+        User user = userMapper.selectById(1);
+        String s = MAPPER.writeValueAsString(user);
+        stringRedisTemplate.opsForValue().set("user:1", s);
+        String s1 = stringRedisTemplate.opsForValue().get("user:1");
+        User user1 = MAPPER.readValue(s1, User.class);
+        System.out.println(user1);
+    }
+
+    @Test
+    void testRedisUtil(){
+        User user = userMapper.selectById(1);
+        redisUtil.setObj("user:1", user);
+        User obj = redisUtil.getObj("user:1", user);
+        System.out.println(obj);
     }
 
 }
